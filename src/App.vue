10b2c4c77d21ca2c07dedc69bcf1fd97c7294eeb
Aleksey Gunchin <a.s.gunchin@gmail.com>
2019-12-06 19:18:05 +0400

    add html code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template lang="pug">
   .wrapper
-    #app
-      h1 hello
+    header
+      .navbar
+        .container
+          .navbar-content
+            router-link.header-logo(to="/") MovieLib
+            .button-burger
+            .navbar-list__wrapper
+              ul.navbar-list
+                li.navbar-item(
+                  v-for="link in linkMenu"
+                  :key="link.title"
+                )
+                  router-link.navbar-link(
+                    :to="link.url"
+                  ) {{ link.title }}
+    .content-wrapper
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      linkMenu: [
+        { title: 'Home', url: '/' },
+        { title: 'Login', url: 'login' },
+        { title: 'Registration', url: '/registration' },
+      ],
+    };
   },
 };
 </script>
 
 <style lang="scss">
 @import './assets/reset.scss';
+
+.navbar-link{
+  display: block;
+  height: 10px;
+  width: 100px;
+}
 </style>
